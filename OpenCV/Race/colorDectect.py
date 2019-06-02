6705dd1185c2cab490d93e57564e9ee183f24bf5
Ryan Keswick <ryank415@gmail.com>
2019-06-02 19:14:02 +1000

    delay frame change

--- OpenCV/Race/colorDectect.py
+++ OpenCV/Race/colorDectect.py
@@ -1,4 +1,4 @@
-#
+#res
 #
 #
 import cv2
@@ -7,9 +7,12 @@ import numpy as np
 def FindBigContour(frame, LinelowerRange, LineupperRange):
     colourFilter = cv2.inRange(frame, LinelowerRange, LineupperRange) 
 
-    kernel = np.ones((5,5), np.uint8)
-    colourFilter = cv2.dilate(colourFilter, kernel)
     
+    kernel = np.ones((5,5), np.uint8)
+    erosion = cv2.erode(colourFilter, kernel, iterations = 1)
+    dilation = cv2.dilate(colourFilter, kernel, iterations = 1)
+    
+
     Contours, _ = cv2.findContours(colourFilter, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     return Contours, colourFilter
