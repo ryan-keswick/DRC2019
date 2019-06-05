a585bf567275b1965387668594649d0d2e9b441c
Ryan Keswick <ryank415@gmail.com>
2019-06-05 14:06:12 +1000

    cali

--- OpenCV/Race/run.py
+++ OpenCV/Race/run.py
@@ -157,8 +157,9 @@ while True:
         print(str(output_steering(diff)))
         ard.write(str.encode((output_steering(diff))))
 # Prints the speed once at the start so this isn't nessacary 
-#        print(str(output_speed(10)))
-#        ard.write((str.encode(output_speed(10))))
+    if frameskip is 12: 
+        print(str(output_speed(10)))
+        ard.write((str.encode(output_speed(10))))
 
     # Lap detection
     delay = delay + 1
