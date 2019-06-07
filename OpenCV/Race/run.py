0dd685a03c840db19f6bc733155e317e4aeb6944
Ryan Keswick <ryank415@gmail.com>
2019-06-07 13:58:55 +1000

    calds

--- OpenCV/Race/run.py
+++ OpenCV/Race/run.py
@@ -189,10 +189,10 @@ while True:
 # Prints the speed once at the start so this isn't nessacary 
     if frameski is 24: 
         frameski = 0
-        if a = 1:
+        if a == 1:
             a = 0
             d = '3'
-        elif a = 0:
+        elif a == 0:
             d = '2'
             a = 1
         print(str(d))
